d4afbea1d8a09d9078ca05cedbdde1aa2b6288f9
krista2811 <krista2811@naver.com>
2017-05-13 20:06:10 +0900

    error error

--- blog/models.py
+++ blog/models.py
@@ -22,10 +22,10 @@ class Comment(models.Model):
     author = models.CharField(max_length = 200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    approved_comment = models.BooleanField(default = False)
+    #approved_comment = models.BooleanField(default = False)
 
-    def approve(self):
+    '''def approve(self):
         self.approved_comment = True
-        self.save()
+        self.save()'''
     def __str__(self):
         return self.text
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.http import *
 from django.shortcuts import render_to_response
-from django.template import RequecstContext
+from django.template import RequestContext
 #from birthdayreminder.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
@@ -56,14 +56,64 @@ def user_logout(request):
 		logout(request)
 	return redirect('/')
 
-def add_comment_to_post(request, pk):
+def add_comment_to_post(request, pk, fuck):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        comment = form.save(commit = False)
-        comment.post = post
-        comment.save()
-        return redirect('blog.views.post_detail', pk=post.pk)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.post = post
+            comment.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def post_approve(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.approve()
+    return redirect('/', pk=post.pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('/', pk=post.pk)
+
+@login_required
+def comment_edit(request, pk):
+    comment = get_object_or_404(Comment, fuck=fuck)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance = comment)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.author = request.user
+            comment.post = get_object_or_404(Post, pk=fuck)
+            comment.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form= PostForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', pk=post_pk)
 # Create your views here.
