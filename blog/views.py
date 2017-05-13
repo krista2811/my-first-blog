d150c78df39b24067730f530a80f11cc4a2e2b6c
krista2811 <krista2811@naver.com>
2017-05-13 15:06:09 +0900

    new Login created. Have to make login html page

--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,15 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'blog/post_detail.html', {'post':post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+def login(request):
+    return render(request, 'blog/login.html', {})
 
 def post_new(request):
     if request.method == "POST":
